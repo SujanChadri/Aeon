b2f1df3ee969cc80e5902c9d35e04777e913950a
SujanChadri <130057029+SujanChadri@users.noreply.github.com>
2024-06-04 19:19:11 +0545

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -209,7 +209,7 @@ def get_readable_message():
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
         msg += f"<b>Name:</b><code> {escape(f'{download.name()}')}</code>\n"
-        msg += f"<b>User:</b><code>{source(download)}</code>\n"
+        msg += f"<b>User:</b><code> {source(download)}</code>\n"
         msg += f"------------------------------------------\n"
         msg += f"<b>{download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING, MirrorStatus.STATUS_PROCESSING]:
