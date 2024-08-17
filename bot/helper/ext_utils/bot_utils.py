a1bebe34d709be6daf157b6f6af4dcceb1dac5ad
SujanChadri <130057029+SujanChadri@users.noreply.github.com>
2024-08-17 14:29:11 +0545

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -88,7 +88,7 @@ class MirrorStatus:
     STATUS_SPLITTING = "Splitting"
     STATUS_CHECKING = "CheckUp"
     STATUS_SEEDING = "Seeding"
-    STATUS_PROCESSING = "Processing"
+    STATUS_PROCESSING = "Adding Metadata"
 
 
 class setInterval:
